6105f28c2e5c4759c1d0f995489aa308ee037143
5hyr4 <6314370@edu.vaniercollege.qc.ca>
2026-04-30 11:50:00 -0400

    updates

---
+++ lia-web4/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import WpApi from './components/WpApi.vue';
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+  <WpApi />
+</template>
+
+<style scoped></style>
